7b5f8b1a850b2c22fe53f9ce33687cceeb0aa17a
githubusefull <mohamedabdelwahidi@gmail.com>
2024-11-27 17:04:28 +0100

    sss

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+let isConnected;
+
+const connectToDatabase = async () => {
+    if (isConnected) return;
+
+    try {
+        const mongodbUri = process.env.MONGODB_URI;
+        if (!mongodbUri) {
+            console.log('MongoDB URI not found in .env file');
+            process.exit(1);
+        }
+
+        const db = await mongoose.connect(mongodbUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = db.connections[0].readyState;
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.log('Error connecting to MongoDB:', err);
+        throw err;
+    }
+};
+
+module.exports = connectToDatabase;
--- server.js
+++ server.js
@@ -1,34 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const Auth = require('./api/routers/Auth'); // Import the user routes
 const Order = require('./api/routers/NewOrder'); // Import the user routes
-const serverless = require('serverless-http'); // Import serverless-http
+const connectToDatabase = require('./db');
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-const port = process.env.PORT || 3000; // Use the port from .env or default to 3000
 
 // Middleware
 app.use(bodyParser.json()); // Middleware to parse incoming JSON requests
 
-// Connect to MongoDB using the URL from .env
-const mongodbUri = process.env.MONGODB_URI;
-if (!mongodbUri) {
-  console.log('MongoDB URI not found in .env file');
-  process.exit(1); // Exit if the URI is missing
-}
 
-mongoose.connect(mongodbUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.log('Error connecting to MongoDB:', err);
-});
 
 // Use the userRouter for the '/users' endpoint
 app.use('/auth', Auth);
@@ -38,4 +22,7 @@ app.use('/order', Order);
 
 
 
-module.exports = serverless(app);
+module.exports = async (req, res) => {
+  await connectToDatabase();
+  app(req, res); // Use express to handle the request
+};
