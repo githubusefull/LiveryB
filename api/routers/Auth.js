c55d3a0b416f768b1394327c2b53cebae00bfd67
githubusefull <mohamedabdelwahidi@gmail.com>
2024-12-02 23:33:45 +0100

    bcrype

--- api/routers/Auth.js
+++ api/routers/Auth.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import User from '../models/Users.js'; // Ensure the schema has a password hashing middleware
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
