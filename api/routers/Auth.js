9a7b72f63e6db8f7ea3511285985ab4e43459115
githubusefull <mohamedabdelwahidi@gmail.com>
2024-12-03 00:01:53 +0100

    Invalids names

--- api/routers/Auth.js
+++ api/routers/Auth.js
@@ -63,13 +63,13 @@ router.post('/login', async (req, res) => {
     // Check if user exists
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: 'Invalid Email' });
     }
 
     // Compare passwords
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: 'Invalid Password' });
     }
 
     // Generate JWT
