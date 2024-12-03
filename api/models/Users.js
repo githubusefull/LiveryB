c8359787cc8623a17d3bdf49c3ec24d987ba1f57
githubusefull <mohamedabdelwahidi@gmail.com>
2024-12-03 01:35:49 +0100

    remove bcry in module

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,4 +1,3 @@
-import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 // Define a schema for the User model
@@ -27,19 +26,9 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Pre-save hook to hash password before storing it
-userSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  }
-  next();
-});
 
-// Method to compare the plain password with the hashed password
-userSchema.methods.comparePassword = function(password) {
-  return bcrypt.compare(password, this.password);
-};
+
+
 
 const User = mongoose.model('User', userSchema);
 
